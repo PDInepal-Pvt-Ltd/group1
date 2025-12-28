e7d30e4ce342e59091fea9e9f55befecdfc3ba19
aayus karki <karki.aayush2003@icloud.com>
2025-12-28 16:37:04 +0545

    feat: add qrcode url in seed script

--- server/src/seed.ts
+++ server/src/seed.ts
@@ -1,7 +1,6 @@
-
-import bcrypt from "bcrypt";
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
+import bcrypt from 'bcrypt';
 import { PrismaClient } from "./generated/prisma/client";
 const connectionString = `${process.env.DATABASE_URL}`
 
@@ -9,29 +8,50 @@ const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
 
 import { Role, TableStatus } from "./generated/prisma/client";
+// import { qrCodeService } from "./common/services/qrCodeService";
 
 
 async function main() {
+  console.log("--- Cleaning Database ---")
+  await prisma.menuItemAllergen.deleteMany();
+  await prisma.menuItem.deleteMany();
+  await prisma.category.deleteMany();
+  // await prisma.table.deleteMany();
   console.log("--- Starting Seed ---");
 
+
+
   // 1. SEED TABLES (Physical layout of your restaurant)
-  const tables = [
-    { name: 'Table 1', seats: 2 },
-    { name: 'Table 2', seats: 4 },
-    { name: 'Table 3', seats: 4 },
-    { name: 'Booth A', seats: 6 },
-    { name: 'Bar 1',   seats: 1 },
-  ];
+  // const tables = [
+  //   { name: 'Table 1', seats: 2 },
+  //   { name: 'Table 2', seats: 4 },
+  //   { name: 'Table 3', seats: 4 },
+  //   { name: 'Booth A', seats: 6 },
+  //   { name: 'Bar 1',   seats: 1 },
+  // ];
 
-  for (const t of tables) {
-    await prisma.table.upsert({
-      where: { name: t.name },
-      update: {},
-      create: { ...t, status: TableStatus.AVAILABLE },
-    });
-  }
-  console.log("✅ Tables seeded");
+  // for (const t of tables) {
+  //   await prisma.table.upsert({
+  //     where: { name: t.name },
+  //     update: {},
+  //     create: { ...t, status: TableStatus.AVAILABLE },
+  //   });
+  // }
+  // console.log("✅ Tables seeded");
 
+//   // After seeding tables
+// const dbtables = await prisma.table.findMany({});
+// for (const table of dbtables) {
+//   const qrCode = await qrCodeService.generateTableQrCode(table.id);
+//   if (qrCode) {
+//     await prisma.table.update({
+//       where: { id: table.id },
+//       data: { qrCodeUrl: qrCode },
+//     });
+//   }
+// }   
+
+const hashedPassword = await bcrypt.hash('password', 10);
   // 2. SEED USERS (Staff members)
   const admin = await prisma.user.upsert({
     where: { email: 'admin@restaurant.com' },
@@ -39,14 +59,14 @@ async function main() {
     create: {
       name: 'Admin User',
       email: 'admin@restaurant.com',
-      password: 'hashed_password_here', // Use bcrypt in production
+      password: hashedPassword,
       role: Role.ADMIN,
     },
   });
   console.log("✅ Admin user seeded");
 
   // 3. FETCH API DATA
-  const response = await fetch('https://dummyjson.com/recipes?limit=15');
+  const response = await fetch('https://dummyjson.com/recipes?limit=50');
   const { recipes } = await response.json();
 
   // 4. SEED MENU, CATEGORIES, AND ALLERGENS
