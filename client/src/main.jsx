2c67022103f58c183b1d56c2319e7d167c3b504c
aayus karki <karki.aayush2003@icloud.com>
2025-12-29 16:14:37 +0545

    feat: Remove routing from main.jsx and integrate Redux store with loading overlay

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Login from './pages/Login';
-import AdminDashboard from './pages/AdminDashboard';
-import Tables from './pages/Tables';
-import MenuPage from './pages/Menu';
-import POSPage from './pages/POS';
-import BillsPage from './pages/Bills';
-import ReservationsPage from './pages/Reservations';
-import SettingsPage from './pages/Settings';
-import KitchenPage from './pages/Kitchen';
+import App from './App';
+import { Toaster } from 'react-hot-toast'
+import store, {persistor} from './store';
+import { LoadingOverlay } from './components/ui/LoadingOverlay';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/tables" element={<Tables />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/pos" element={<POSPage />} />    
-        <Route path="/bills" element={<BillsPage />} />     
-        <Route path="/reservations" element={<ReservationsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/kitchen" element={<KitchenPage />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={<LoadingOverlay />} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
